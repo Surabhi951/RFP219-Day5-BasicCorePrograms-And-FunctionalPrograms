b4eec73b84c296e0a538fe6c18c9a4ec05dc86bc
Surabhi951 <surabhibhagat95@gmail.com>
2022-10-23 23:38:11 +0530

    Added: Basic core programs

---
+++ src/com/bridgelabz/BasicCorePrograms/EvenOddNumber.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.BasicCorePrograms;
+
+import java.util.Scanner;
+
+public class EvenOddNumber {
+
+    public static void main(String[] args) {
+        System.out.println("Enter Number: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        EvenOddNumber evenOddNumber = new EvenOddNumber();
+        evenOddNumber.print(n);
+    }
+        public void print(int n) {
+            if (n % 2 == 0) {
+                System.out.println("Number is even");
+            } else {
+                System.out.println("Number is odd");
+            }
+        }
+}
+
